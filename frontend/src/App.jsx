2e6de0ff9e4057debcbc458bdb6ea7db5053f38c
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2025-11-23 14:19:41 -0400

    added the missing css file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Excalidraw } from '@excalidraw/excalidraw'
+import "@excalidraw/excalidraw/index.css";
 
 function App() {
 
   return (
     <>
-    
+    <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
+      <div style={{ height: "90vh", width: "100%" }}>
+        <Excalidraw  theme='light'/>
+      </div>
     </>
   )
 }
